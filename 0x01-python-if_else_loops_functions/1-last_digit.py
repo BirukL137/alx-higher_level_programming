0b11a906a40dad6c5f31ab968423e867c6830c3f
Biruk Lemma <buraf13k@gmail.com>
2022-08-23 11:50:52 -0700

    1. The last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-number = int(number)
-num = number
-d = 1000
-if num < 0:
-    neg_num = num
-    num *= -1
+
+if number < 0:
+    buf = number * -1
 else:
-    neg_num = num
-for i in range(3):
-    num %= d
-    d /= 10
-num = int(num)
-if neg_num < 0:
-    num *= -1
-    if num > 5:
-        print(f"Last digit of {number:d} is {num:d} and is greater than 5")
-    elif num == 0:
-        print(f"Last digit of {number:d} is {num:d} and is 0")
-    elif num < 6 and not 0:
-        print(f"Last digit of {number:d} is {num:d} and is less than 6 and not 0")
-else:
-    if num > 5:
-        print(f"Last digit of {number:d} is {num:d} and is greater than 5")
-    elif num == 0:
-        print(f"Last digit of {number:d} is {num:d} and is 0")
-    elif num < 6 and not 0:
-        print(f"Last digit of {number:d} is {num:d} and is less than 6 and not 0")
+    buf = number
+
+last_digit = buf % 10
+if number < 0:
+    last_digit = last_digit * -1
+
+if last_digit == 0:
+    print(f"Last digit of {number:d} is 0 and is 0")
+
+elif last_digit > 5:
+    print(f"Last digit of {number:d} is {last_digit:d} and is greater than 5")
+
+elif last_digit < 6:
+    print(f"Last digit of {number:d} is {last_digit:d} and is less than 6 and not 0")
