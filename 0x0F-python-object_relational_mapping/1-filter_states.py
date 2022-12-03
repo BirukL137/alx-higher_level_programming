15ccfb0436c7980556ed76781338e85b07adfae6
BirukL137 <buraf13k@gmail.com>
2022-12-03 00:42:59 -0800

    1. Filter states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,6 +4,7 @@
 import MySQLdb
 import sys
 
+
 def list_N():
     '''lists all states with a name that starts with N'''
     username = sys.argv[1]
@@ -15,8 +16,8 @@ def list_N():
     db = MySQLdb.connect(host=host, user=username, passwd=password,
                          db=db_name, port=port)
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name regexp "^N.*" ' + 'ORDER BY states.id ASC')
-    
+    cur.execute('SELECT * FROM states WHERE name regexp "^N.*" ' +
+                'ORDER BY states.id ASC')
     result = cur.fetchall()
     cur.close()
     db.close()
@@ -24,6 +25,7 @@ def list_N():
         for row in result:
             if row[1][0] == "N":
                 print(row)
+
 
 if __name__ == "__main__":
     list_N()
