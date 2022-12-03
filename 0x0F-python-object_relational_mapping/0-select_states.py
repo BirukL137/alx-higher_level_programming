e7d33b75930d4e2bdeb8772332b3e30072e873d8
BirukL137 <buraf13k@gmail.com>
2022-12-03 00:20:24 -0800

    0. Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -16,11 +16,11 @@ def list_all():
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC;")
     query_rows = cur.fetchall()
+    cur.close()
+    conn.close()
     if query_rows:
         for row in query_rows:
             print(row)
-    cur.close()
-    conn.close()
 
 if __name__ == '__main__':
     list_all()
