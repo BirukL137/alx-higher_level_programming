9fcd8f8ef4c7b64fb0d0c4231dedc222c55d6466
BirukL137 <buraf13k@gmail.com>
2022-12-16 08:41:00 -0800

    9. My GitHub!

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" A Python script uses the GitHub API to display your id """
+
+from sys import argv
+from requests import get
+from requests.auth import HTTPBasicAuth
+
+if __name__ == '__main__':
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    res = get("https://api.github.com/user", auth=auth)
+    print(res.json().get('id'))
